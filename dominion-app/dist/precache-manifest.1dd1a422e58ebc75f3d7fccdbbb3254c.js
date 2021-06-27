ed14ea948d9ed2747d113c92af80a66f05cdf9cc
Yan Gobeil <yan.gobeil@decathlon.com>
2021-06-27 14:52:38 -0400

    add dist folder

---
+++ dominion-app/dist/precache-manifest.1dd1a422e58ebc75f3d7fccdbbb3254c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198111bff639ee1de2cd",
+    "url": "/css/chunk-vendors.57060246.css"
+  },
+  {
+    "revision": "154a2311fb5d63a29c2c635f043c3a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3ddcbe8d0f3bfc7d628",
+    "url": "/js/app.74d32f99.js"
+  },
+  {
+    "revision": "198111bff639ee1de2cd",
+    "url": "/js/chunk-vendors.851a7d30.js"
+  },
+  {
+    "revision": "637df66b6818493d62d870d0d699195e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
